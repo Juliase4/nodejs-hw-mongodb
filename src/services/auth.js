73a4bc88c21ecd17060ffd122ef533131eafc19b
Yulia Serheieva <Julia.sergeeva93@gmail.com>
2024-08-07 04:41:02 +0300

    add step 4

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
 import bcrypt from 'bcrypt';
 import createError from 'http-errors';
+import { randomBytes } from 'crypto';
 import { User } from '../db/models/user.js';
+import { Session } from '../db/models/session.js';
+import { THIRTY_DAYS, FIFTEEN_MINUTES } from '../constants/constants.js';
 
 export async function registerUser(payload) {
   const hashedPassword = await bcrypt.hash(payload.password, 10);
@@ -14,5 +17,31 @@ export async function registerUser(payload) {
   return await User.create({
     ...payload,
     password: hashedPassword,
+  });
+}
+
+export async function loginUser(payload) {
+  const user = await User.findOne({ email: payload.email });
+  console.log(user);
+  if (!user) {
+    throw createError(404, 'User not found');
+  }
+
+  const isPasswordEqual = await bcrypt.compare(payload.password, user.password);
+  if (!isPasswordEqual) {
+    throw createError(401, 'Unauthorized');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await Session.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + THIRTY_DAYS),
   });
 }
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -10,3 +10,13 @@ export const registerValidationSchema = Joi.object({
   }),
   password: Joi.string().required(),
 });
+
+export const loginValidationSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'string.email': 'email is not valid',
+    'any.required': 'email is required',
+  }),
+  password: Joi.string().required().messages({
+    'any.required': 'password is required',
+  }),
+});
