f961919facae902653a95e8c11fd3684bf7f623d
Yulia Serheieva <Julia.sergeeva93@gmail.com>
2024-08-14 00:47:47 +0300

    add routes, controllers, services

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,19 @@
 import bcrypt from 'bcrypt';
 import createError from 'http-errors';
+import jwt from 'jsonwebtoken';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import handlebars from 'handlebars';
+import { sendMail } from '../utils/sendMail.js';
 import { randomBytes } from 'crypto';
 import { User } from '../db/models/user.js';
 import { Session } from '../db/models/session.js';
-import { THIRTY_DAYS, FIFTEEN_MINUTES } from '../constants/constants.js';
+import {
+  THIRTY_DAYS,
+  FIFTEEN_MINUTES,
+  SMTP,
+  TEMPLATE_DIR,
+} from '../constants/constants.js';
 
 export async function registerUser(payload) {
   const hashedPassword = await bcrypt.hash(payload.password, 10);
@@ -85,4 +95,70 @@ export async function refreshSession({ sessionId, refreshToken }) {
     userId: session.userId,
     ...newSession,
   });
+}
+
+export async function requestResetToken(email) {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createError(404, 'User not found');
+  }
+
+  const jwtToken = jwt.sign(
+    {
+      sub: user._id,
+      email: email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '5m',
+    },
+  );
+
+  const templateFile = path.join(TEMPLATE_DIR, 'reset-password-email.html');
+
+  const resetPasswordTemplatePath = await fs.readFile(templateFile, {
+    encoding: 'utf-8',
+  });
+
+  const template = handlebars.compile(resetPasswordTemplatePath);
+
+  const html = template({
+    name: user.name,
+    link: `${process.env.APP_DOMAIN}/reset-password?token=${jwtToken}`,
+  });
+
+  await sendMail({
+    from: SMTP.FROM,
+    to: email,
+    subject: 'Reset your password',
+    html: html,
+  });
+}
+
+export async function resetPassword(password, token) {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+
+    const user = await User.findOne({
+      _id: decoded.sub,
+      email: decoded.email,
+    });
+
+    if (!user) {
+      createError(404, 'User not found');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.findByIdAndUpdate(user._id, { password: hashedPassword });
+  } catch (error) {
+    if (
+      error.name === 'TokenExpiredError' ||
+      error.name === 'JsonWebTokenError'
+    ) {
+      throw createError(401, 'Token is expired or invalid.');
+    }
+    throw error;
+  }
 }
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -20,3 +20,12 @@ export const loginValidationSchema = Joi.object({
     'any.required': 'password is required',
   }),
 });
+
+export const requestResetEmailSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+export const resetPasswordSchema = Joi.object({
+  password: Joi.string().required(),
+  token: Joi.string().required(),
+});
